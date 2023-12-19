cb0b0b71d9068370893f4ad898aff9498e229250
Anton <antonsimd@gmail.com>
2023-12-19 12:54:45 +1100

    Initial Commit

---
+++ MindfulnessPlus/MindfulnessPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindfulnessPlusApp.swift
+//  MindfulnessPlus
+//
+//  Created by Anton Simdyankin on 19/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct MindfulnessPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
